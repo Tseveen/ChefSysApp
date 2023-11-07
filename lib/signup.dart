d65a85472bba5c10d015faa74688c3eab495cb3a
Tseveen <tseveenbna@gmail.com>
2023-11-07 11:14:05 +0800

    test

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUp> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Бүртгүүлэх'),
-        backgroundColor: Colors.lightBlue,
+        title: Text('Бүртгүүлэх'),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Овог',  // Add a comma here
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Нэвтрэх нэр',
               ),
             ),
-
-            Container(
-              width: 300,
-              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 16),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Овог',
-                ),
+            SizedBox(height: 10),
+            
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
               ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Нууц үг',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your sign-up logic here
+                // You can access user input using _usernameController.text, _emailController.text, and _passwordController.text
+              },
+              child: Text('Бүртгүүлэх'),
             ),
           ],
         ),
